654d316a83bd66ca92c50520ee65718026dde8f3
karandas777 <karandas.21.08.98@gmail.com>
2020-10-22 09:57:29 +0530

    added react-spring

--- src/components/elements/ImgCard.js
+++ src/components/elements/ImgCard.js
@@ -1,37 +1,50 @@
 import React, { Component } from "react";
+import { Spring } from "react-spring/renderprops";
 
 export default class ImgCard extends Component {
   render() {
-    const { id,urls, alt_description, links, user } = this.props.img;
+    const { id, urls, alt_description, links, user } = this.props.img;
 
     return (
-      <div className="content-item bg-custom-1 p-1 rounded shadow-sm">
-        <img src={urls.regular} alt={alt_description} className="w-100 rounded" />
-        <div className="px-0 pt-1">
-          <div className="p-0 m-1 d-flex justify-content-between">
-            <img
-              src={user.profile_image.medium}
-              className="rounded-pill shadow-sm mr-2 mb-1"
-              height="30"
-              width="30"
-              alt="user"
-            />
-            <span className="text-custom-3 mb-0 mt-1">{user.name}</span>
+      <Spring
+        from={{ transform: "translate(0px,150px)" }}
+        to={{ transform: "translate(0px,0px)" }}
+      >
+        {(props) => (
+          <div style={props}>
+            <div className="content-item bg-custom-1 p-1 rounded shadow-sm">
+              <img
+                src={urls.regular}
+                alt={alt_description}
+                className="w-100 rounded"
+              />
+              <div className="px-0 pt-1">
+                <div className="p-0 m-1 d-flex justify-content-between">
+                  <img
+                    src={user.profile_image.medium}
+                    className="rounded-pill shadow-sm mr-2 mb-1"
+                    height="30"
+                    width="30"
+                    alt="user"
+                  />
+                  <span className="text-custom-3 mb-0 mt-1">{user.name}</span>
+                </div>
+                <div className="d-flex justify-content-between p-0 m-1">
+                  <span className="code-font text-custom-3 mb-0">{id}</span>
+                  <a
+                    className="text-decoration-none"
+                    href={`${links.download}?force=true`}
+                  >
+                    <span className="badge bg-custom-2 text-custom-3 shadow-sm h5 mb-0 rounded">
+                      <i className="fa fa-arrow-down"></i>
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="d-flex justify-content-between p-0 m-1">
-            <span className="code-font text-custom-3 mb-0">
-              {id}
-            </span>
-            <a
-              className="text-decoration-none"
-              href={`${links.download}?force=true`}
-            ><span className="badge bg-custom-2 text-custom-3 shadow-sm h5 mb-0 rounded">
-                  <i className="fa fa-arrow-down"></i>
-            </span>
-            </a>
-          </div>
-        </div>
-      </div>
+        )}
+      </Spring>
     );
   }
 }
--- src/components/elements/Logo.js
+++ src/components/elements/Logo.js
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
+import { Spring} from "react-spring/renderprops";
 
 export default class Logo extends Component {
   render() {
     const {firstText, lastText ,info} = this.props;
     return (
-      <div className="my-5">
-      <div className="mb-2 text-center pageTitle sty-font">
-        <span className="text-custom-3 mr-1">{firstText}</span>{lastText}
-        <span className="text-custom-3 ml-1">.</span>
-      </div>
-      <div className="small text-custom-3 text-center">
-          {info}
-      </div>
-      </div>
+      <Spring from={{transform: "translate(0px,150px)"}} to={{transform: "translate(0px,0px)"}}>
+          {
+            props => (
+            
+              <div style={props} className="my-5">
+              <div className="mb-2 text-center pageTitle sty-font">
+                <span className="text-custom-3 mr-1">{firstText}</span>{lastText}
+                <span className="text-custom-3 ml-1">.</span>
+              </div>
+              <div className="small text-custom-3 text-center">
+                  {info}
+              </div>
+              </div>
+             
+            )
+          }
+      </Spring>
     );
   }
 }
